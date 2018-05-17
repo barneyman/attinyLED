6ce6e960ca7f7fc48960db689a75686c2f46eb4e
barneyman <barnaby.james.flint@gmail.com>
2018-05-17 15:23:42 +1000

    moved circQ to libraries

--- attinyLED.ino
+++ attinyLED.ino
@@ -26,117 +26,14 @@
 #endif
 
 
-template <int _CIRCQSIZE, int _CIRQSIZEBITS>
-class circQueueT 
-{
-
-protected:
-
-	byte m_data[_CIRCQSIZE];
-	// let the compiler do the work
-	volatile byte readCursor : _CIRQSIZEBITS, writeCursor : _CIRQSIZEBITS;
-	volatile byte readCursorState : _CIRQSIZEBITS, writeCursorState : _CIRQSIZEBITS;
-	volatile byte availBytes, availBytesState;
-
-public:
-
-	circQueueT()
-	{
-		reset();
-	}
-
-	unsigned available()
-	{
-		return availBytes;
-	}
-
-	int size()
-	{
-		return _CIRCQSIZE;
-	}
-
-	int space()
-	{
-		return _CIRCQSIZE - available();
-	}
-
-	void reset()
-	{
-		readCursor = writeCursor = availBytes=0;
-		readCursorState = writeCursorState = availBytesState = 0;
-	}
-
-	void pushState()
-	{
-		readCursorState = readCursor;
-		writeCursorState = writeCursor;
-		availBytesState = availBytes;
-	}
-
-	void popState()
-	{
-		readCursor=readCursorState;
-		writeCursor = writeCursorState;
-		availBytes = availBytesState;
-	}
-
-
-
-	byte read()
-	{
-		byte ret = -1;
-
-		{
-			// remember int state
-			uint8_t tmp = SREG;
-			// clear (may be cleared already)
-			cli();
-
-			if (readCursor != writeCursor)
-			{
-				// 
-				availBytes--;
-				ret = m_data[readCursor++];
-			}
-
-			// reinstate interrupts, maybe
-			SREG = tmp;
-		}
-
-		return ret;
-	}
-
-	bool write(byte data)
-	{
-		bool ret = false;
-
-		{
-			// remember int state
-			uint8_t tmp = SREG;
-			// clear (may be cleared already)
-			cli();
-
-			// relying on bits width math
-			if ((readCursor - 1) != writeCursor)
-			{
-				m_data[writeCursor++] = data;
-				availBytes++;
-				ret = true;
-			}
-			// reinstate interrupts, maybe
-			SREG = tmp;
-		}
-
-		return ret;
-	}
-
-};
 
 
 #ifdef _USE_MACROS
 #include <twi.h>
 // the max in the TWI code i think, so lets not exceed that
 #define MAX_MACRO	TWI_RX_BUFFER_SIZE
+
+#include <circQ.h>
 
 circQueueT<32,5> macro;
 #endif
